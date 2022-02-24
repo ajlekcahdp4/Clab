9b90c41f353233fab0fbc807697ec1225179a182
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-02-24 18:19:37 +0300

    move graph

--- graph.py
+++ graph.py
@@ -7,8 +7,8 @@ import mnk
 #GNUplot
 #Векторная графика .svg
 def DrawGraph (N, x, y, a, b, ad, bd, name1, name2):
-    line = np.array([a*z + b for z in range ( int(max(x) * (1 + 0.1) + 1))])
-
+    X = np.linspace (min(x)* 0.95, max(x)* 1.05, 100)
+    Y = np.array([a*x_c + b for x_c in X])
     fig, ax = plt.subplots(figsize=(8, 6))
 
     ax.set_xlabel(name1, fontsize=14)
@@ -19,8 +19,9 @@ def DrawGraph (N, x, y, a, b, ad, bd, name1, name2):
     ax.grid(which="major", linewidth=1.2)
     ax.grid(which="minor", linestyle="--", color="gray", linewidth=0.5)
 
-    ax.plot(line, c = 'blue', label="График по МНК")
+    ax.plot(X, Y, c = 'blue', label="График по МНК")
     ax.scatter(x, y, c = 'red', label="Измеренные точки")
+    #plt.plot ([200, 250, 270, 304], [0.001, 0.004, 0.009, 0.0095], color = "red")
     
     
     ax.legend()
