48d36729d0867b2a71dce4b602eafaf7ef98874a
ajlekcahdp4 <alex.rom23@mail.ru>
2022-03-26 15:57:50 +0300

    right filename

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import input
 #x, y, a, b, ad, bd = input.InputLinearLSM ("data/data_lsm.txt")
 #G.DrawGraphLine (x, y, a, b, ad, bd, "x", "y")
 
-x, y, a = input.InputPolinomLSM ("data/data_lsm_pol.txt")
+x, y, a = input.InputPolinomLSM ("data/data_lsm_pol.res")
 G.DrawGraphPol (x, y, a, "x", "y")
